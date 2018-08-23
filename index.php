298c1f48d2070731f02fa08e1ee7d033bc4da208
Corey Schafer <CoreyMSchafer@gmail.com>
2018-08-23 09:48:32 +0530

    I create my faverit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>List Of Foods</title>
+</head>
+<body>
+	<li>Rice</li>
+	<li>Fish</li>
+	<li>Chiken</li>
+	<li>Banana</li>
+	<li>Mango</li>
+	<li>Ice Cream</li>
+
+</body>
+</html>
